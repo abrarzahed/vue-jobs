8d7ad5e3b5f635b5620ed4d896306c46c8e0d646
Abrar Hussen Zahed <abrarzahed6986@gmail.com>
2021-03-04 13:53:30 +0600

    commited

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Jobs from '../views/jobs/Jobs.vue'
+import Jobdetail from '../views/jobs/Jobdetail.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -10,10 +14,29 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'Jobdetail',
+    component: Jobdetail,
+    props: true
+  },
+  // redirect to jobs
+  {
+    path: '/all-jobs',
+    redirect: '/Jobs'
+  },
+  // catchAll 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Home Page</h1>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus quia iusto molestias sed non praesentium, ut quae consequuntur! Non, aspernatur. Aut reprehenderit sint error non amet temporibus! Iste iure ipsa cupiditate tempore repellat! Laudantium, rem totam asperiores atque animi harum dolorem possimus? Quibusdam recusandae exercitationem nostrum laudantium magni veniam ullam corporis deserunt asperiores mollitia vitae laborum nihil excepturi iure doloribus sed voluptas, minus velit sequi, cupiditate quia sunt! Laboriosam fuga voluptatum, dolore minus in vitae hic voluptatibus est corporis harum eius earum vero fugiat quod praesentium blanditiis nemo architecto? Laboriosam facere odio quas? Aperiam ut voluptatum nemo deleniti cupiditate quo!</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium suscipit ut obcaecati dolor deserunt, quaerat ratione excepturi dolores beatae quisquam architecto eaque doloremque at. Architecto asperiores placeat, laudantium modi tempora possimus quos eaque omnis quaerat doloremque natus recusandae. Similique ab alias error nemo consequuntur, voluptate perferendis labore est voluptatem quibusdam. Iusto praesentium quidem perspiciatis recusandae quos minus rerum sequi id fugit eius, et nisi nihil dolorum debitis aspernatur ea optio fugiat? Magni doloremque voluptas animi iste quod voluptatem itaque asperiores quos, delectus harum vero impedit dolor? Molestias dolor porro alias, recusandae voluptate est rem itaque molestiae temporibus id aut aliquam.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
